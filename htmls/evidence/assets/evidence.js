687b5b3f1b07a21ced5971f21243b67c2a56d1ea
kennyfe <kennyfe@tcd.ie>
2014-08-21 10:44:30 +0100

    update evidence 0 with animation, links, favicon etc

---
+++ htmls/evidence/assets/evidence.js
@@ -0,0 +1,31 @@
+window.onload=function()
+{ 
+	setViewedEvidence(); 
+			
+    document.getElementById("animation").className += " load";
+	//$("#animation").toggleClass( "load" );
+		
+	setTimeout(function(){flashIcon(false)}, 1500);
+	setTimeout(function(){flashIcon(true)}, 1750);
+	setTimeout(function(){flashIcon(false)}, 2000);
+	setTimeout(function(){flashIcon(true)}, 2250);
+	setTimeout(function(){flashIcon(false)}, 2500);
+	//setTimeout(function(){flashIcon(true)}, 3000);
+	
+	//document.getElementById("inventory-corner").className += "afteranimation";
+	//$("#inventory-corner").toggleClass( "afteranimation" );
+};
+	
+function flashIcon(flashon)
+{
+	if(flashon == false)
+	{
+		document.getElementById("inventory-corner").style.backgroundImage = "url(../images/icon_inventory_hover.png)";
+		flashon = true;
+	}
+	else if(flashon == true)
+	{
+		document.getElementById("inventory-corner").style.backgroundImage = "url(../images/icon_inventory.png)";
+		flashon = false;
+	}
+}
