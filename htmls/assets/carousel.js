ec3adafa6108b0177729e8a391b7a390706e9094
Kyle Murphy <kyle-murphy89@live.com>
2014-08-23 09:08:44 +0100

    Improved performance + fixed quick nav issue
    Hash linking has been changed so that instead of directly linking to the
    element it instead uses the hash value to pan the carousel the same way
    as the buttons on the side. This fixes the issue where the scripted css
    and the html fragments were causing conflicts with each other. Also, I
    have assigned jQuery dom traversals to variables because traversing the
    dom over and over with jq is very wasteful, you should only do it once
    for every selector you use

--- htmls/assets/carousel.js
+++ htmls/assets/carousel.js
@@ -1,14 +1,15 @@
 $(function(){
   
-	var elems = $('#carousel li'),
+	var $elems = $('#carousel li'),
+	$carousel = $('#carousel'),
     speed = 400,
     evidence = getViewedEvidence(),
-    
+    diff = 0,
     currentSelection = 0,
     timelineSelection = 0,
-    $itemCount = elems.length,
+    $itemCount = $elems.length,
     $timeline = $('#timeline li'),
-    $itemWidth = elems.css('width').split('px')[0];
+    $itemWidth = $elems.css('width').split('px')[0];
     buildCarousel(evidence);
 
 
@@ -19,47 +20,20 @@ $(function(){
 	$('#carousel li:odd').css({'z-index' : '2',
 								'opacity' : '0.7'});
 
+window.onhashchange = function hashPan(){
 
-window.onhashchange = function unfuckeverything(){
+	//just in case
 	var $top = $('#quicknav');
 	$(document).scrollTop($top);
+
 	currentSelection = parseInt((location.hash).split('#')[1]);
 
-	console.log("currentSelection is: " + currentSelection);
-
-	var el = elems.eq(currentSelection),
-
-	offset = el.offset(),
-	posXOffset = offset.left - $(window).scrollLeft(),
-	elWidth = el.width(),
-	viewportWidth = $(window).width();
-
-	console.log("half the viewport is: " + viewportWidth/2);
-	console.log("posXOffset + elWidth/2 is: " + (posXOffset + (elWidth/2)));
-	console.log("currentSelection is " + currentSelection);
+	var el = $elems.eq(currentSelection);
 	el.css('background-color', 'red');
 
-	var diff = (posXOffset + (elWidth/2)) - (viewportWidth/2);
-
-	if(diff > 0){
-
-		console.log("difference is: " + diff);
-
-		$('#carousel').animate({
-			marginLeft: '-' + diff + 'px'
+		$carousel.animate({
+			left: '-' + (currentSelection*$itemWidth) + 'px'
 		}, 200);
-	
-	}else if(diff < 0){
-
-		console.log("difference is: " + diff);
-
-		$('#carousel').animate({
-			marginLeft: '-' + diff + 'px'
-		}, 200);
-	}else{
-		//do nothing
-	}
-	
 };
 
 
@@ -79,10 +53,11 @@ function updateElement(key, value)
 		
 		if(value == true)
 		{
-			elems.eq(key).css({'background-image' : 'url("timeline-previews/' + index + '.jpg")',
+			$elems.eq(key).css({'background-image' : 'url("timeline-previews/' + index + '.jpg")',
 									'-webkit-filter' : 'sepia(70%)'} );
+
 		}else{
-			elems.eq(key).css({'background-image' : 'url("timeline-previews/lock_v2.png"' + ')',
+			$elems.eq(key).css({'background-image' : 'url("timeline-previews/lock_v2.png"' + ')',
 								'background-size' : '100%',
 								'background-repeat' : 'repeat-x',
 								'background-position' : 'center'
@@ -92,67 +67,10 @@ function updateElement(key, value)
 
 
 
-/*
-function updateElement(key, value){
-
-	if(value == true){
-		elems.eq(key).css("background-color", "red");
-	}else{
-		elems.eq(key).css("background-color", "yellow");
-	}
-
-}*/
 
 function panCarousel(e){
 	
 	
-	/*
-	if(e.data.type == 'quicknav'){
-		//do this
-		var incrementIndex = 0;
-		switch(e.target.id){
-			case 'First':
-				incrementIndex = 0;
-				break;
-			case 'Second':
-				incrementIndex = 10;
-				break;
-			case 'Third':
-				incrementIndex = 20;
-				break;
-			case 'Fourth':
-				incrementIndex = 30;
-				break;
-			case 'Fifth':
-				incrementIndex = 40;
-				break;
-			case 'Sixth':
-				incrementIndex = 50;
-				break;
-			case 'Seventh':
-				incrementIndex = 60;
-				break;
-			case 'Eight':
-				incrementIndex = 70;
-				break;
-			case 'Ninth':
-				incrementIndex = 80;
-				break;
-			case 'Tenth':
-				incrementIndex = 90;
-				break;
-			default: 
-				break;
-		}
-		currentSelection = (incrementIndex)%$itemCount;
-		$('#carousel').animate({
-			left: '-' +(currentSelection*$itemWidth) + 'px'
-		}, speed);
-	}
-	else{*/
-	//e.data is the object which holds and processes passed in arguments
-	//e.data.direction holds the direction : forward key-value pair
-
 	if(e.data.direction == 'forward'){
 	
 
@@ -160,12 +78,12 @@ function panCarousel(e){
 		(currentSelection + 4)%($itemCount);
 		if(currentSelection < 0)
 			currentSelection = currentSelection+94;
-		console.log(currentSelection);
+		
 		//currentSelection = (currentSelection+4)%$itemCount;
 		//modulos operator makes it so that currentSelection resets to 0 if it reaches end of array
-		$('#carousel').animate({
+		$carousel.animate({
 			
-			marginLeft: '-' + (currentSelection*$itemWidth) + 'px'
+			left: '-' + (currentSelection*$itemWidth) + 'px'
 		}, speed);
 		
 		}else{
@@ -173,10 +91,10 @@ function panCarousel(e){
 		currentSelection = (currentSelection===0) ? ($itemCount-1) : (currentSelection-4)%($itemCount);
 		if(currentSelection < 0)
 			currentSelection = currentSelection+94;
-		console.log(currentSelection);
-		$('#carousel').animate(		
+		
+		$carousel.animate(		
 		{
-		marginLeft : '-' + (currentSelection*$itemWidth) + 'px'
+		left : '-' + (currentSelection*$itemWidth) + 'px'
 		}, speed);
 
 }
@@ -194,7 +112,7 @@ $('#navPrev').bind('click', {direction: 'backward',
 
 $('#quicknav a').bind('mouseover', changeYear);
 
-$('#carousel').bind('mouseover', changeYear);
+$carousel.bind('mouseover', changeYear);
 
 
 function changeYear(e){
@@ -219,10 +137,10 @@ function changeYear(e){
 			timelineSelection = 5;
 			break;
 		case 'Outbreak':
-			timelineSeleciton = 6;
+			timelineSelection = 6;
 			break;
 		case 'Quarantine':
-			timelineSeleciton = 7;
+			timelineSelection = 7;
 			break;
 		case 'Evac':
 			timelineSelection = 8;
@@ -234,13 +152,12 @@ function changeYear(e){
 			break;
 	}
 
-		if($('#timeline li').eq(timelineSelection).className != 'selected')
+		if($timeline.eq(timelineSelection).className != 'selected')
 		{
-		$('#timeline li').eq(timelineSelection).siblings()
+		$timeline.eq(timelineSelection).siblings()
 		.addClass('notSelected').removeClass('selected');	
-		$('#timeline li').eq(timelineSelection).addClass('selected').removeClass('notSelected');
+		$timeline.eq(timelineSelection).addClass('selected').removeClass('notSelected');
 		
 		}
 	}
 });
-
